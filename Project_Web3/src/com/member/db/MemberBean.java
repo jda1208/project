6f986738c3465ef1cb120e3c3b03d2ca30dafcd6
jda <ekdo6226@naver.com>
2015-04-23 14:10:30 +0900

    db연동

---
+++ Project_Web3/src/com/member/db/MemberBean.java
@@ -0,0 +1,57 @@
+package com.member.db;
+
+public class MemberBean {
+	private String user_id;
+	private String user_email;
+	private String user_name;
+	private String user_password;
+	private String user_phonenum;
+	private String user_menu;
+	private String gradenum;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public String getUser_phonenum() {
+		return user_phonenum;
+	}
+	public void setUser_phonenum(String user_phonenum) {
+		this.user_phonenum = user_phonenum;
+	}
+	public String getUser_menu() {
+		return user_menu;
+	}
+	public void setUser_menu(String user_menu) {
+		this.user_menu = user_menu;
+	}
+	public String getGradenum() {
+		return gradenum;
+	}
+	public void setGradenum(String gradenum) {
+		this.gradenum = gradenum;
+	}
+	
+	
+
+}
